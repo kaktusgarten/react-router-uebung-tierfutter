16adc676aa8143829001f3d38bb5a2b36adc5b28
Marco Rehberg <marcorehberg@gmx.net>
2025-09-24 15:39:22 +0200

    Alles bis auf slugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router";
+import MainLayout from "./layout/MainLayout";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesPage from "./pages/ArticlesPage";
+import ArticlesInfo from "./components/articlesPage/AriclesInfo";
+import Articles from "./components/articlesPage/articles";
+import AdminLayout from "./layout/AdminLayout";
+import AdminHomePage from "./pages/admin/AdminHomePage";
+import SettingsPage from "./pages/admin/SettingsPage";
+import DashboardPage from "./pages/admin/DashboardPage";
+import ArticleDetails from "./components/articlesPage/ArticleDetails";
+
 function App() {
   return (
-    <>
-      <div class="border">Hallo App</div>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/articles" element={<ArticlesPage />}>
+          <Route index element={<ArticlesInfo />} />
+          <Route path="/articles/overview" element={<Articles />} />
+          <Route
+            path="/articles/article-details"
+            element={<ArticleDetails />}
+          />
+        </Route>
+        <Route path="*" element="Not found..."></Route>
+      </Route>
+
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminHomePage />} />
+        <Route path="/admin/settings" element={<SettingsPage />} />
+        <Route path="/admin/dashboard" element={<DashboardPage />} />
+      </Route>
+    </Routes>
   );
 }
 
