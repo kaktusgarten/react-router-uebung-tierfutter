6d1f1899b71e652f7ee57d5d4ad57575543c9bef
Marco Rehberg <marcorehberg@gmx.net>
2025-09-25 12:00:19 +0200

    Pfade verbessert

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router";
-import MainLayout from "./layout/MainLayout";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ArticlesPage from "./pages/ArticlesPage";
-import ArticlesInfo from "./components/articlesPage/AriclesInfo";
-import Articles from "./components/articlesPage/Articles";
-import AdminLayout from "./layout/AdminLayout";
-import AdminHomePage from "./pages/admin/AdminHomePage";
-import SettingsPage from "./pages/admin/SettingsPage";
-import DashboardPage from "./pages/admin/DashboardPage";
-import ArticleDetails from "./components/articlesPage/ArticleDetails";
+import {
+  MainLayout,
+  HomePage,
+  AboutPage,
+  ArticlesPage,
+  ArticlesInfo,
+  Articles,
+  AdminLayout,
+  AdminHomePage,
+  SettingsPage,
+  DashboardPage,
+  ArticleDetails,
+} from ".";
 
 function App() {
   return (
